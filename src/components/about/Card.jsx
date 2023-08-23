a99e1dde499ae72a5fe8d8b094abcdaaf51eba93
AxolotlJ-Dev <kalelcg.eg@gmail.com>
2023-08-22 21:52:50 -0600

    feat:
    icons/card listos todo funciona, probable arreglar el overflow

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -1,27 +1,54 @@
+"use client";
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import React from "react";
 
-export const Card = ({ Img, Name }) => {
+export const Card = ({ Img, Name, linkD, linkG, linkL }) => {
   return (
     <div className="border p-3 flex flex-col items-center max-w-[350px] max-h-[505px] mx-auto">
-      <img
+      <Image
         src={Img}
+        width={350} 
+        height={350}
         className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[325px] lg:h-[325px] xl:w-[350px] xl:h-[350px] mb-4 sm:mb-6 md:mb-7 border-2 border-white"
         style={{ borderRadius: "100%" }}
         alt={Name}
       />
 
-      <h3 className="text-xl text-white text-center mb-4"> { Name} </h3>
+      <h3 className="text-xl text-white text-center mb-4"> {Name} </h3>
 
       <div className="flex space-x-2">
-        <span className="w-8 h-8 flex items-center justify-center bg-gray-400 rounded-full text-white">
-          D
+        <span className="w-full h-full flex items-center justify-center">
+          <a href={`https://discordapp.com/users/${linkD}`}>
+            <FontAwesomeIcon
+              icon={faDiscord}
+              size="3x"
+              className="hover:text-[#7289da]"
+            />
+          </a>
         </span>
-        <span className="w-8 h-8 flex items-center justify-center bg-gray-400 rounded-full text-white">
-          G
+        <span className="w-full h-full flex items-center justify-center">
+          <a href={linkG}>
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="3x"
+              className="hover:text-[#5d5d5d]"
+            />
+          </a>
         </span>
-        <span className="w-8 h-8 flex items-center justify-center bg-gray-400 rounded-full text-white">
-          L
+        <span className="w-full h-full flex items-center justify-center">
+          <a href={linkL}>
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="3x"
+              className="hover:text-[#0e76a8]"
+            />
+          </a>
         </span>
       </div>
     </div>
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,14 +1,19 @@
-"use client"
+"use client";
 import React from "react";
 import "../../app/globals.css";
 import { useRouter } from "next/navigation";
+
+// img
+import DanUser from "../../assets/DanUser.jpeg";
+import WaterUser from "../../assets/WaterUser.jpeg";
+import AxolotlUser from "../../assets/AxolotlUser.jpeg";
 
 import { Card } from "@/components/about/Card";
 
 const Page = () => {
   const route = useRouter();
   return (
-    <div className=" bg-black w-screen h-screen flex justify-center  text-white overflow-auto">  
+    <div className="bg-black text-white overflow-scroll">
         <main
           className="absolute top-12 left-12 border border-white px-4 py-2 z-50"
           // aun falta Hover
@@ -18,28 +23,60 @@ const Page = () => {
         >
           <p>regresar</p>
         </main>
+        <div className="w-screen h-screen overflow-scroll ">
 
         <div className="flex flex-col container mx-auto px-4 my-auto justify-center ">
           {/* Título */}
-          <p className="text-2xl sm:text-4xl md:text-5xl text-center mb-10 sm:mb-16 mt-6 sm:mt-12">Desarrolladores</p>
-
+          <p className="text-2xl sm:text-4xl md:text-5xl text-center mb-10 sm:mb-16 mt-6 sm:mt-12">
+            Desarrolladores
+          </p>
 
           {/* Cartas */}
-        
+
           <div className="flex flex-wrap justify-center items-center">
+            {/*
+            Estructura de links/icons
+            Img
+            Name
+            LinkD: Id
+            linkG
+            LinkL
+          */}
+
             <div className=" p-4">
-              <Card />
+              <Card
+                Img={DanUser}
+                Name="Dan"
+                linkD="600492246256844800"
+                linkG="https://github.com/DandevMS"
+                linkL="https://www.linkedin.com/in/danilo-macea/"
+              />
             </div>
+
             <div className=" p-4">
-              <Card />
+              <Card
+                Img={AxolotlUser}
+                Name="Axolotl"
+                linkD="670423461104975903"
+                linkG="https://github.com/AxolotlJ-Dev"
+                linkL="https://www.linkedin.com/in/javier-cabrales-1ba86b241/"
+              />
             </div>
+
             <div className=" p-4">
-              <Card />
+              <Card
+                Img={WaterUser}
+                Name="Water"
+                linkD="775889013420195871"
+                linkG="https://github.com/watercubz/"
+                linkL="https://www.linkedin.com/in/watercubz/"
+              />
             </div>
           </div>
-          </div>
-        
+        </div>
       </div>
+            {/* NutriaSoft  */}
+    </div>
   );
 };
 
