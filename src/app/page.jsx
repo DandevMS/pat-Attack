80fc9432a7491207d578b0c9d26d2df650c4e584
AxolotlJ-Dev <kalelcg.eg@gmail.com>
2023-08-24 04:56:22 -0600

    style:
    agregacion de titulo, mejoras en la card y arreglo del about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
-"use client"
-// import Bush from "@/visuals/Bush";
+"use client";
 import Cloud from "@/components/home/Cloud";
 import { useRouter } from "next/navigation";
 
@@ -7,8 +6,16 @@ export default function Home() {
   const route = useRouter();
 
   return (
-    <div className="interfaze flex items-center justify-center h-screen w-screen bg-[#030c29] ">
+    <div className="interfaze flex flex-col items-center justify-center h-screen w-screen bg-[#030c29] ">
       <Cloud />
+      <p
+        className="TitleH text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center z-50 text-white mb-12 md:mb-24 lg:mb-32 xl:mb-36"
+        style={{
+          fontFamily: "var(--font-retro)",
+        }}
+      >
+        pat Attack
+      </p>
       <div className=" btn flex flex-col">
         <button
           className="pixel2 "
@@ -19,9 +26,14 @@ export default function Home() {
         >
           Start
         </button>
-        <button className="pixel2" onClick={() => {
-          route.push(`/about/about`);
-        }}  >About</button>
+        <button
+          className="pixel2"
+          onClick={() => {
+            route.push(`/about/about`);
+          }}
+        >
+          About
+        </button>
       </div>
     </div>
   );
--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -12,11 +12,13 @@ export const Card = ({ Img, Name, linkD, linkG, linkL }) => {
   return (
     <div className="border p-3 flex flex-col items-center max-w-[350px] h-500 max-h-[505px] mx-auto ">
       <Image
-              src={Img}
-              className="w-80 h-80  mb-4 sm:mb-6 md:mb-7 border-2 border-white rounded-[100%] "  
-              alt={Name}
-              style={{objectFit: 'cover'}}
-            />
+        src={Img}
+        width={350} 
+        height={350}
+        className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[325px] lg:h-[325px] xl:w-[350px] xl:h-[350px] mb-4 sm:mb-6 md:mb-7 border-2 border-white"
+        style={{ borderRadius: "100%" }}
+        alt={Name}
+      />
       <h3 className="text-xl text-white text-center mb-4"> {Name} </h3>
 
       <div className="flex space-x-2">
